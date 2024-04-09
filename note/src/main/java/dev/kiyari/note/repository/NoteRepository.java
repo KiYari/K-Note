1b519399c321c30aa2b08f42e55cfc9b9fc112e9
Kim Arthur <termrdz@gmail.com>
2024-04-09 12:46:16 +1000

    changed docker-compose

--- note/src/main/java/dev/kiyari/note/repository/NoteRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.kiyari.note.repository;
-
-import dev.kiyari.note.model.entity.Note;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoteRepository extends CrudRepository<Note, Long> {
-}
