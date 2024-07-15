32765ec61f6bbda3b8293d552496579e35ef2bf0
Arthur <49246305+KiYari@users.noreply.github.com>
2024-07-15 12:53:52 +1000

    Feature/frnt 61 (#70)
    * added Logo&go to profile

--- core/KnoteUI/src/layout/header/Header.tsx
+++ core/KnoteUI/src/layout/header/Header.tsx
@@ -3,11 +3,19 @@ import HeaderProps from "@/layout/header/header.props";
 import styles from "./header.module.css";
 import BasicComponent from "@/component/basicComponent/BasicComponent";
 import Link from "next/link";
+import {Typography} from "@mui/material";
 
 const Header:FC<HeaderProps> = ({className, style, ...props}) => {
     return(
         <BasicComponent className={`${styles.main} ${className}`} style={style} {...props}>
-            HEADER_LINE
+            <Link href={'/'} className={styles.homeHolder}>
+                <Typography variant="h5">
+                    <Typography variant="h3">
+                        K
+                    </Typography>
+                    Note
+                </Typography>
+            </Link>
 
             <Link href={'/profile'} className={styles.profileHolder}>
                 profile
